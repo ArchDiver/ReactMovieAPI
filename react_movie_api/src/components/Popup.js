dba854c9f14d0a959dcc6182ed7702a8aaecce13
LSC <lelandscrawford@gmail.com>
2021-01-01 19:57:54 -0500

    adding server

--- react_movie_api/src/components/Popup.js
+++ react_movie_api/src/components/Popup.js
@@ -6,12 +6,14 @@ function Popup({ selected, closePopup }) {
 			<div className="content">
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2> <br></br>
 				<p className="director">Director: {selected.Director}</p>
-				<p className="rating">Rating: {selected.imdbRating}</p>
+				<p className="rating">IMDB Rating: {selected.imdbRating}</p>
 				<div className="plot">
 					<img src={selected.Poster} alt='Poster'/>
 					<p>{selected.Plot}</p>
 				</div>
 				<button className="close" onClick={closePopup}>Close</button>
+				<h3>Like &#128077;</h3> 
+				<h3>Dislike &#128078;</h3>
 			</div>
 		</section>
 	)
