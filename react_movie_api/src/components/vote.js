c734395d365d5566301391ed56920e37bfbec803
LSC <lelandscrawford@gmail.com>
2021-01-11 20:54:24 -0500

    cleaning the code

--- react_movie_api/src/components/vote.js
+++ react_movie_api/src/components/vote.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import axios from 'axios'
 
-// function Vote( selected, like, db ) {
-//     if (like === true){
-//         axios.patch(db + '/' + selected.likes + 1)
-//     }
-//     else if (like === false){
-//         axios.patch(db + '/' + selected.dislikes + 1)
-//     }
-// }
+function Vote( selected, like, db ) {
+    if (like === true){
+        axios.patch(db + '/' + selected.likes + 1)
+    }
+    else if (like === false){
+        axios.patch(db + '/' + selected.dislikes + 1)
+    }
+}
 export default Vote;
 
