91e9c8162d65d6048f622df511d3cb8c132702a6
LSC <lelandscrawford@gmail.com>
2021-01-13 20:01:17 -0500

    cleaning the code

--- react_movie_api/src/components/movie.js
+++ react_movie_api/src/components/movie.js
@@ -2,16 +2,16 @@
 import axios from 'axios';
 
 
-// function Movie( selected,  db ) {
-//     if (selected.imdbID in db.imdbID){
-//         axios.all([
-//             axios.get(db.imdbID.likes),
-//             axios.get(db.imdbID.dislikes),
-//             axios.get(db.imdbID.ratings)
-//         ])
-//         .then(axios.spread(( likes, dislikes) => {
-//         return { likes, dislikes};
-//         }))
+function Movie( selected,  db ) {
+    if (selected.imdbID in db.imdbID){
+        axios.all([
+            axios.get(db.imdbID.likes),
+            axios.get(db.imdbID.dislikes),
+            axios.get(db.imdbID.ratings)
+        ])
+        .then(axios.spread(( likes, dislikes) => {
+        return { likes, dislikes};
+        }))
     }
     else {
         axios.post(db, {imdbID: selected.imdbID, title:selected.title, dislikes:0, likes:0, rating:0});
